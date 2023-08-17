3f86fb03267aab4a9a15d509b207b0bd03f22fd0
Jawahir01 <714591@student.cityofbristol.ac.uk>
2023-08-17 21:14:01 +0000

    Prepare the code for Deployment to Heroku

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -11,6 +11,18 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+
+db = SQLAlchemy(app)
 
 # The reason the taskmanager being imported last, is because the 'routes' file, that we're about
 # to create, will rely on using both the 'app' and 'db' variables defined above.
